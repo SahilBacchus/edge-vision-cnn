750dfb690dff818efa586461700a64476d9decaf
Sahil Bacchus <sahilbacchus@gmail.com>
2025-12-24 23:46:53 -0700

    feat: add initial implementation of EdgeCNN architecture

--- model.py
+++ model.py
@@ -7,13 +7,13 @@ import torch.nn as nn
 
 
 class DepthwiseSeparableConv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
         super().__init__()
 
         # Depthwise convolution --> applies a single convolution per input channel (groups=in_channel)
         self.depthwise = nn.Conv2d(
             in_channels, in_channels, 
-            kernel_size=kernel_size, stride=stride, padding=1, groups=in_channels, bias=False
+            kernel_size=kernel_size, stride=stride, padding=padding, groups=in_channels, bias=False
         )
 
         # Pointwise convolution --> applies 1x1 convolution to combine outputs of depthwise conv
@@ -31,3 +31,43 @@ class DepthwiseSeparableConv(nn.Module):
         x = self.bn(x)
         x = self.relu(x)
         return x
+    
+
+class EdgeCNN(nn.Module): 
+    def __init__(self, num_classes=10): 
+        super().__init__()
+
+        # Intial standard Convolution --> "stem" are usually the intial simple set of layers
+        self.stem = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(32), 
+            nn.ReLU(inplace=True)
+        )
+
+        # Depthwwsie seperable blocks
+        self.features = nn.Sequential(
+            # 32x32
+            DepthwiseSeparableConv(32, 64, stride=1),
+
+            # 16x16
+            DepthwiseSeparableConv(64, 128, stride=2), 
+            DepthwiseSeparableConv(128, 128, stride=1),
+
+            # 8x8 
+            DepthwiseSeparableConv(128, 256, stride=2),
+            DepthwiseSeparableConv(256, 256, stride=1)
+        )
+
+        # Standard feed forward network to do classification based on features
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Flatten(), 
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.stem(x)
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
